857882d3e761afcd2abe164a8b7a623dff8442e7
AarinGuleria <aaringuleria2310@gmail.com>
2025-01-29 11:46:33 +0530

    Add About component with GSAP scroll animations and integrate into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import About from "./components/About";
 import Hero from "./components/hero";
 import "./index.css";
 
@@ -5,8 +6,7 @@ function App() {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Hero />
-
-      <section className="z-0 min-h-screen bg-blue-500" />
+      <About />
     </main>
   );
 }
