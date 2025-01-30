ae7b4a74817e3dcece845bf1a717326a344b72b1
AarinGuleria <aaringuleria2310@gmail.com>
2025-01-30 21:32:08 +0530

    Add Navbar component and integrate into App for improved navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import About from "./components/About";
 import Hero from "./components/hero";
+import Navbar from "./components/Navbar";
 import "./index.css";
 
 function App() {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Navbar />
       <Hero />
       <About />
     </main>
